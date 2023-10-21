5331bccd1f88010c958ee12fceec90e95c01f915
marcus <marcus.burdine@gmail.com>
2023-10-21 13:40:10 -0400

    first commit

---
+++ digimon/api/digimon.js
@@ -0,0 +1,19 @@
+
+
+let digimon = [
+{
+    name:"",
+    level:"",
+    type:"",
+    attribute:"",
+    health:0,
+    speed:0,
+    skills:[[]],
+    numSkills:0,
+    profile:""
+
+}
+
+    
+
+]
