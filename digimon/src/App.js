cd384d3191aa82eacd68f5dcac5d87c18b0df290
marcus <marcus.burdine@gmail.com>
2023-10-23 20:26:37 -0400

    second commit

--- digimon/src/App.js
+++ digimon/src/App.js
@@ -3,7 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import { getUser } from "./utilities/service/user";
 import SignUp from "./pages/signUp/signUp";
 import LogIn from './pages/login/login'
+import Home from "./pages/home/home"
 import DigiDex from './pages/digiDex/digiDex'
+import Game from "./pages/game/game"
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -14,10 +16,10 @@ export default function App() {
       <Routes>
       <Route path='/' element={ <SignUp setUser={setUser} />} />
       <Route path='/logIn' element={ <LogIn setUser={setUser} />} />
+      <Route path='/home' element={ <Home setUser={setUser} />} />
       <Route path='/digiDex' element={ <DigiDex setUser={setUser} />} />
+      <Route path='/game' element={ <Game setUser={setUser} />} />
       </Routes>
-
-      
     </>
 
   )
